fb87e626da6b2d3efe43c52ca21d097d0da725ee
langletamelie <amelie.langlet@hotmail.com>
2018-06-26 19:09:11 +0200

    correction

--- assets/js/js3exo2.js
+++ assets/js/js3exo2.js
@@ -4,3 +4,4 @@ function hide() {
 function show(){
   document.getElementById('text').style.visibility = 'visible';
 }
+// .style.diplay = 'block' ou = 'none' cache le texte et remonte le contenu du dessous alors que visibilty cache le texte mais laisse un espace vide
